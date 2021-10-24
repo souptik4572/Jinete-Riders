db64aaeb28fd027b91124d037e40147ecab63923
souptik4572 <souptiksarkar4572@gmail.com>
2021-10-25 00:37:41 +0530

    changed update route to update-profile route

--- api/passenger/urls.py
+++ api/passenger/urls.py
@@ -4,5 +4,5 @@ from .views import register_passenger, login_passenger, edit_passenger_data
 urlpatterns = [
     path('register/', register_passenger, name='api.register_passenger'),
     path('login/', login_passenger, name='api.login_passenger'),
-    path('update/', edit_passenger_data, name='api.edit_passenger_data'),
+    path('update-profile/', edit_passenger_data, name='api.edit_passenger_data'),
 ]
