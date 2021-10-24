0a62fad7f287560648351db3d32664806a9adf72
souptik4572 <souptiksarkar4572@gmail.com>
2021-10-24 11:01:02 +0530

    created register route for Passenger

--- api/passenger/urls.py
+++ api/passenger/urls.py
@@ -1,11 +1,12 @@
 from rest_framework import routers
 from django.urls import path, include
-from .views import PassengerViewSet
+from .views import PassengerViewSet, register_passenger
 
 router = routers.DefaultRouter()
 
 router.register(r'', PassengerViewSet)
 
 urlpatterns = [
+    path('register/', register_passenger, name='api.register_passenger'),
     path('', include(router.urls))
 ]
--- api/passenger/views.py
+++ api/passenger/views.py
@@ -1,8 +1,39 @@
 from rest_framework import viewsets
+from django.http import JsonResponse
 from .serializers import PassengerSerializer
 from .models import Passenger
+from django.views.decorators.csrf import csrf_exempt
+import json
 
 # Create your views here.
+
+
+@csrf_exempt
+def register_passenger(request):
+    if request.method == 'PUT':
+        data = json.loads(request.body)
+        try:
+            name = data['name']
+            email = data['email']
+            phone = data['phone']
+            password = data['password']
+            city = data['city']
+            country = data['country']
+            profile_image = data['profile_image']
+            print(data)
+            return JsonResponse({
+                'success': True
+            })
+        except KeyError:
+            return JsonResponse({
+                'success': False,
+                'message': 'Some data missing'
+            })
+    else:
+        return JsonResponse({
+            'success': False,
+            'message': 'Please only provide a PUT request with necessary data'
+        })
 
 
 class PassengerViewSet(viewsets.ModelViewSet):
