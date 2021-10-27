1911c7d5f7a3aedb87baabff839d2918b83a61dc
souptik4572 <souptiksarkar4572@gmail.com>
2021-10-27 15:11:39 +0530

    register driver token model in driver admin

--- api/driver/admin.py
+++ api/driver/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Driver
+from .models import Driver, Token
 
 # Register your models here.
 admin.site.register(Driver)
+admin.site.register(Token)
