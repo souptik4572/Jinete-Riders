edf537786137f04b8e559133f54c3bcb6fa51aed
souptik4572 <souptiksarkar4572@gmail.com>
2021-11-04 22:25:38 +0530

    removed password from driver serializer

--- api/driver/serializers.py
+++ api/driver/serializers.py
@@ -5,5 +5,5 @@ from .models import Driver
 class DriverSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Driver
-        fields = ('id', 'name', 'email', 'phone', 'password',
+        fields = ('id', 'name', 'email', 'phone',
                   'city', 'country', 'profile_image', 'rating', 'car_name', 'car_number', 'car_image', 'driving_license_no')
